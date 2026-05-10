c579cebd99576a3d2b0c9b05dc2b327911b743de
adams262 <adam.sobel11@gmail.com>
2026-05-10 14:50:36 +0200

    Sidebar update

--- PZPP_Grupa5/Views/MainPage.xaml.cs
+++ PZPP_Grupa5/Views/MainPage.xaml.cs
@@ -1,11 +1,37 @@
 ﻿using PZPP_Grupa5.ViewModels;
+
 namespace PZPP_Grupa5.Views;
 
 public partial class MainPage : ContentPage
 {
+    
+    private const uint AnimationDuration = 250;
+
     public MainPage(MainViewModel viewModel)
     {
         InitializeComponent();
         BindingContext = viewModel;
+    }
+
+    
+    private async void OnHamburgerClicked(object sender, EventArgs e)
+    {
+        MenuOverlay.IsVisible = true;
+
+        await Task.WhenAll(
+            SidebarMenu.TranslateTo(0, 0, AnimationDuration, Easing.CubicOut),
+            MenuOverlay.FadeTo(0.6, AnimationDuration, Easing.CubicOut)
+        );
+    }
+
+    
+    private async void OnOverlayTapped(object sender, EventArgs e)
+    {
+        await Task.WhenAll(
+            SidebarMenu.TranslateTo(-300, 0, AnimationDuration, Easing.CubicIn),
+            MenuOverlay.FadeTo(0, AnimationDuration, Easing.CubicIn)
+        );
+
+        MenuOverlay.IsVisible = false;
     }
 }
